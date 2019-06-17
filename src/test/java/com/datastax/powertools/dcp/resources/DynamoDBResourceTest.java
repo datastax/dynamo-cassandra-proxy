a83be9bb0c985eeacf068083a2d3636d8c19d82d
phact <estevezsebastian@gmail.com>
2019-06-17 16:28:38 -0400

    remove stale tests

--- src/test/java/com/datastax/powertools/dcp/resources/DynamoDBResourceTest.java
+++ src/test/java/com/datastax/powertools/dcp/resources/DynamoDBResourceTest.java
@@ -1,15 +1,5 @@
 package com.datastax.powertools.dcp.resources;
 
-import org.testng.annotations.Test;
-
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.Map;
-
-
 /*
  *
  * @author Sebastián Estévez on 12/6/18.
@@ -19,39 +9,4 @@ import java.util.Map;
 
 public class DynamoDBResourceTest {
 
-    private static WebTarget webTarget = ClientBuilder.newClient().target("http://localhost:8080");
-
-    @Test
-    public void testAsyncCreateTable() {
-        int result = post("/table", "", null);
-        assert(result==0);
-    }
-
-    public int post(String path, String body, Map<String, String> queryParams) {
-
-        WebTarget target= webTarget.path(path);
-
-        Response response;
-
-        if (queryParams != null){
-            for (Map.Entry<String, String> paramEntry : queryParams.entrySet()) {
-                target = target.queryParam(paramEntry.getKey(), paramEntry.getValue());
-            }
-        }
-        try {
-            if (!body.equals("")) {
-                response = target.request(MediaType.APPLICATION_JSON).post(Entity.entity(body, MediaType.APPLICATION_JSON));
-            } else {
-                response = target.request(MediaType.APPLICATION_JSON).get();
-            }
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            return -255;
-        }
-
-        int status = response.getStatus();
-
-        return status;
-    }
 }
